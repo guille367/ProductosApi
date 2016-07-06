01746f08b469b0454a174733220b3a48f2a06fa6
elwis <ponceandresguillermo@hotmail.com>
2016-07-06 03:20:35 +0000

    Pruebas varias

--- client/js/controller/CategoryController.js
+++ client/js/controller/CategoryController.js
@@ -1,13 +1,45 @@
 angular.module('myApp')
 
     .controller('CategoryController', function($scope,CategoryService){
-      
-      $scope.pruebaRequest = function () {
-            
-            var res = CategoryService.findAll().then(function(d){
-            })
-            
+        
+        $scope.selectedCategory
+        $scope.selectedIndex = -1
+        
+        $scope.selectCategory = function(index,item){
+            $scope.selectedCategory = item
+            $scope.selectedIndex = index
         }
         
+        $scope.cleanSelection = function (){
+            $scope.selectedCategory = {}
+            $scope.selectedIndex = -1
+        }
+        
+        $scope.save = function () {
+            //var res = CategoryService.findAll().then(function(d){
+            //})
+        }
+        $scope.update = function (c) {
+            
+        }
+        $scope.delete = function(){
+            alert($scope.selectedCategory.description)
+        }
+    
+        
+        CategoryService.findAll().then(function(d){
+            $scope.categories = d    
+        })
+        
+          /*$scope.categories = [{
+              name:'aaa',
+              description:'www'
+          },{
+              name:'bbb',
+              description:'eee'
+          },{
+              name:'ccc',
+              description:'rrr'
+          }]*/
         
     })
--- client/js/services/CategoryService.js
+++ client/js/services/CategoryService.js
@@ -5,12 +5,24 @@ angular.module('myApp')
         let path = 'https://productosapi-ponceandresguille.c9users.io'
         
         let extractData = function(res){
-            alert(res.data);
+            return res.data
         };
         
         this.findAll = function(){
             return $http.get(path + '/categorias')
                 .then(extractData)
+        }
+        
+        this.addCategory = function(c){
+            
+        }
+        
+        this.updateCategory = function(c){
+            
+        }
+        
+        this.deleteCategory = function(categoryID){
+            
         }
         
     })
