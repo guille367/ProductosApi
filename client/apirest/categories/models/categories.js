5d118267ba8d0937e2a236b80255b409ca83bc6b
elwis <ponceandresguillermo@hotmail.com>
2016-07-11 02:27:18 +0000

    terminado el router de productos

--- client/apirest/categories/models/categories.js
+++ client/apirest/categories/models/categories.js
@@ -17,21 +17,24 @@ exports.add = function (category) {
 }
 
 exports.delete = function (id) {
-  
   var oid = conn.ObjectID(id)
-  console.log('delete: ' + oid)
+  
   conn.connect(function(err, db) {
+      db.open()
       db.collection(DB_COLLECTION)
         .findOneAndDelete({ _id: oid })      
   })
+  
 }
 
 exports.update = function(category){
   conn.connect(function(err, db) {
-    
       var oid = conn.ObjectID(category._id)
-      console.log(oid)
+      console.log('update: ' + oid);
       db.collection(DB_COLLECTION)
-        .updateOne( { _id: oid } , { $set: category }, { upsert: true })
+        .findOneAndUpdate( { _id: oid },
+        { $set: { name: category.name, description: category.description } },
+        { upsert: true },function(){ db.close() } )
+        
   })
 }
---
+++ client/apirest/categories/models/products.js
@@ -0,0 +1,18 @@
+var conn = require('./connection')
+var DB_COLLECTION = 'products'
+
+exports.get = function(){
+    
+}
+
+exports.add = function(product){
+    
+}
+
+exports.update = function(product){
+    
+}
+
+exports.delete = function(product){
+    
+}
